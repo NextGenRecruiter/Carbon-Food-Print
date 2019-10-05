546637813c69d349c0281c098f704677ecb6817f
Edith Emmings <emmings.edith@gmail.com>
2019-10-05 18:31:59 -0500

    db changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React, { Component} from 'react';
 import './App.css';
 // import Header from '../Header/Header'
 import Home from '../Home/Home'
-import Summary from '../Dashboard/Dashboard'
+import Summary from '../Summary/Summary'
 import Dashboard from '../Dashboard/Dashboard'
 import Social from '../Social/Social'
 import { HashRouter as Router, Route} from 'react-router-dom'
