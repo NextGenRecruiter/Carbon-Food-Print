449fb9db73e8521f7a97dd641a68ad50d6ee2482
Edith Emmings <emmings.edith@gmail.com>
2019-10-05 16:02:52 -0500

    better comments

--- server/routes/food.router.js
+++ server/routes/food.router.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 
-// hit get('/food')
-// get back your days and total emissions
+// hit get('/food/name')
+// get single food item and metrics
 router.get('/:name', (req, res) => {
     const queryText = `SELECT * FROM "foods" WHERE "food_item"=$1;`;
     pool.query(queryText, [req.params.name])
