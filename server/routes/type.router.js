a19bea75b153ed411cf268b6cca52a3eb63b4a32
Edith Emmings <emmings.edith@gmail.com>
2019-10-05 16:07:42 -0500

    gets foods of a type excluding single one

--- server/routes/type.router.js
+++ server/routes/type.router.js
@@ -6,12 +6,8 @@ const pool = require('../modules/pool');
 // hit get('/type')
 // get similar foods for comparison
 router.get('/', (req, res) => {
-    let food = {
-        type: 'Protein',
-        name: 'Eggs'
-    }
     const queryText = `SELECT * FROM "foods" WHERE "food_type"=$1 AND "food_item"<>$2;`;
-    pool.query(queryText, [food.type, food.name])
+    pool.query(queryText, [req.body.type, req.body.name])
         .then((result) => { res.send(result.rows); })
         .catch((err) => {
             console.log('Error completing SELECT movie query', err);
