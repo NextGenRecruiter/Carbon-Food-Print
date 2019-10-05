7f99b9a73f333a431db2985942a809850f9a3e0e
Edith Emmings <emmings.edith@gmail.com>
2019-10-05 17:13:34 -0500

    days routers written

--- server/routes/days.router.js
+++ server/routes/days.router.js
@@ -3,21 +3,22 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 
-// hit get('/days/list')
+// hit get('/days/date')
 // get all food items from a particular day
-// router.get('/list', (req, res) => {
-//     const queryText = `SELECT * FROM "foods" WHERE "food_item"=$1;`;
-//     pool.query(queryText, [req.params.name])
-//         .then((result) => { res.send(result.rows); })
-//         .catch((err) => {
-//             console.log('Error completing SELECT movie query', err);
-//             res.sendStatus(500);
-//         });
-// });
+router.get('/:date', (req, res) => {
+    const queryText = `SELECT "day", array_agg("food") AS "foods"
+                        FROM "days" WHERE "day"=$1 GROUP BY "day";`;
+    pool.query(queryText, [req.params.date])
+        .then((result) => { res.send(result.rows); })
+        .catch((err) => {
+            console.log('Error completing SELECT movie query', err);
+            res.sendStatus(500);
+        });
+});
 
-// hit get('/days/sum')
+// hit get('/days')
 // get the sum of emissions for each day
-router.get('/sum', (req, res) => {
+router.get('/', (req, res) => {
     const queryText = `SELECT "days".day, sum("foods".emissions_per_day_kg) FROM "foods" 
                         JOIN "days" ON "foods".food_item = "days".food GROUP BY "days".day;`;
     pool.query(queryText)
