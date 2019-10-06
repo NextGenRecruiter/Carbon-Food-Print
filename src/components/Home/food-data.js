4e2565942f931c0ffc1bddadb86be50e033a52c7
Kim Mai <Kim.Mai@chrobinson.com>
2019-10-05 19:45:08 -0500

    merge

--- src/components/Home/food-data.js
+++ src/components/Home/food-data.js
@@ -1,37 +1,15 @@
 import React, { Component, useState } from 'react';
 import almond from '../../assets/almond-svgrepo-com.svg'
 import Axios from 'axios';
-import { Link } from 'react-router-dom'
 
-class FoodData extends Component {
-    state = {
-        foods: [],
-        totals: []
-    }
-    componentDidMount = () => {
-        // list of all foods in the current day
-        Axios.get('/days/foods/' + this.props.date).then(response => {
-            console.log(response)
-            this.setState({
-                ...this.state,
-                foods: response.data[0].foods
-            })
-        }).then(error => {
-            console.log(error)
-        })
-        // list of the totals comparisons
-        Axios.get('/days/totals/' + this.props.date).then(response => {
-            console.log(response)
-            this.setState({
-                ...this.state,
-                totals: response.data[0]
-            })
-        }).then(error => {
-            console.log(error)
-        })
-    }
 
-    render() {
+const fakeData = {
+    foodItem: 'Apple',
+    amount: '1/day',
+    emissions: '0.0329',
+    miles: '0.0877',
+    showers: '0.2192'
+};
 
 export const FoodData = (props) => {
     const [item, setItem] = useState([]);
